747c245b50fa933103d6a8e43ab3e12201b7c8c2
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2024-11-28 11:52:02 +0200

    implemented the Hero.jsx and added the gsap animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,18 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react';
 import Button from "./Button.jsx";
 import {TiLocationArrow} from "react-icons/ti";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
 
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     const [loadedVideo, setLoadedVideo] = useState(0);
 
     const totalVideos = 3;
@@ -23,10 +29,66 @@ const Hero = () => {
         setCurrentIndex(upcomingVideoIndex);
     }
 
+    useEffect(() => {
+        if(loadedVideo === totalVideos -1){
+            setLoading(false);
+        }
+    }, [loadedVideo])
+
+    useGSAP(() => {
+        if(hasClicked){
+            gsap.set('#next-video', {visibility: 'visible'});
+            gsap.to('#next-video', {
+                transformOrigin: 'center center',
+                scale: 1,
+                width: '100%',
+                height: '100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart: () => nextVideoRef.current.play(),
+              }
+            );
+            gsap.from("#current-video", {
+                transformOrigin: "center center",
+                scale: 0,
+                duration: 1.5,
+                ease: "power1.inOut",
+            });
+        }
+    }, {dependencies: [currentIndex], revertOnUpdate: true});
+
+    useGSAP(() => {
+        gsap.set("#video-frame", {
+            clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+            borderRadius: "0% 0% 40% 10%",
+        });
+        gsap.from("#video-frame", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            borderRadius: "0% 0% 0% 0%",
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: "#video-frame",
+                start: "center center",
+                end: "bottom center",
+                scrub: true,
+            },
+        });
+    });
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return (
         <div className={"relative h-dvh w-screen overflow-x-hidden"}>
+            {loading && (
+                <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+                    {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+                    <div className="three-body">
+                        <div className="three-body__dot"></div>
+                        <div className="three-body__dot"></div>
+                        <div className="three-body__dot"></div>
+                    </div>
+                </div>
+            )}
             <div id="video-frame" className={"relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"}>
                 <div>
                     <div
