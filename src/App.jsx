43985aee283212bc89608b0795607e76b512711e
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2024-11-29 09:05:57 +0200

    implemented the about.jsx and added the gsap animation on the image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero.jsx";
+import About from "./components/about.jsx";
 
 
 const App = () => {
@@ -7,7 +8,7 @@ const App = () => {
   return (
       <main className={"relative min-h-screen w-screen overflow-x-hidden"}>
         <Hero />
-          <section className={"z-0 min-h-screen bg-blue-500"}></section>
+          <About />
       </main>
   )
 }
---
+++ src/components/animated-title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AnimatedTitle() {
+    return (
+        <div className={"mt-5 text-center text-4xl uppercase leading-[.8] md:text-[6rem]"}>
+            Disc<b>o</b>ver the world's <br/> largest shared <b>a</b>dventure
+        </div>
+    )
+}
+
+export default AnimatedTitle
